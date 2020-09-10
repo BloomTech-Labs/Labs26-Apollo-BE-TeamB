4aca61b8e5241d28a267b7e80a8ea432963d086b
caleb <bargercj20@hotmail.com>
2020-09-10 13:21:11 -0700

    implemented the findAll questions endpoint

--- src/main/java/com/lambdaschool/apollo/repository/QuestionRepository.java
+++ src/main/java/com/lambdaschool/apollo/repository/QuestionRepository.java
@@ -3,6 +3,9 @@ package com.lambdaschool.apollo.repository;
 import com.lambdaschool.apollo.models.Question;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends CrudRepository<Question, Long> {
 
+    List<Question> findAll();
 }
