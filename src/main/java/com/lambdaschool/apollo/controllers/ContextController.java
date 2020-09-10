fa57eafb639acedf7c47016d8f6ca2ecc4ae52a0
caleb <bargercj20@hotmail.com>
2020-09-10 12:49:29 -0700

    getContextById enpoint working

--- src/main/java/com/lambdaschool/apollo/controllers/ContextController.java
+++ src/main/java/com/lambdaschool/apollo/controllers/ContextController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,13 @@ public class ContextController {
     @GetMapping(value = "/contexts", produces = {"application/json"})
     public ResponseEntity<?> getAllContexts() {
         List<Context> myContexts = contextService.findAll();
-        return new ResponseEntity<>( myContexts,HttpStatus.OK);
+        return new ResponseEntity<>(myContexts,HttpStatus.OK);
     }
 
-
+    @GetMapping(value = "/contexts/{contextId}", produces = {"application/json"})
+    public ResponseEntity<?> getContextById(@PathVariable Long contextId) {
+        Context context = contextService.findById(contextId);
+        return new ResponseEntity<>(context, HttpStatus.OK);
+    }
 
 }
