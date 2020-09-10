86dcf789ec975cf57754f74c61df3268ce2a8e8b
caleb <bargercj20@hotmail.com>
2020-09-10 12:35:20 -0700

    created contextController and implemented a /contexts endpoint

---
+++ src/main/java/com/lambdaschool/apollo/controllers/ContextController.java
@@ -0,0 +1,29 @@
+package com.lambdaschool.apollo.controllers;
+
+import com.lambdaschool.apollo.models.Context;
+import com.lambdaschool.apollo.services.ContextService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping("/contexts")
+public class ContextController {
+
+    @Autowired
+    private ContextService contextService;
+
+    @GetMapping(value = "/contexts", produces = {"application/json"})
+    public ResponseEntity<?> getAllContexts() {
+        List<Context> myContexts = contextService.findAll();
+        return new ResponseEntity<>( myContexts,HttpStatus.OK);
+    }
+
+
+
+}
