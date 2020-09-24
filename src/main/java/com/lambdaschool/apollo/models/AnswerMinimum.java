c0919162b8000da8758c96ff6c161fad7015bc9a
Caleb <bargercj20@hotmai.com>
2020-09-24 11:40:57 -0700

    implemented a body feild on the minimumAnswer

--- src/main/java/com/lambdaschool/apollo/models/AnswerMinimum.java
+++ src/main/java/com/lambdaschool/apollo/models/AnswerMinimum.java
@@ -1,4 +1,20 @@
 package com.lambdaschool.apollo.models;
 
 public class AnswerMinimum {
+    private String body;
+
+    public AnswerMinimum() {
+    }
+
+    public AnswerMinimum(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
