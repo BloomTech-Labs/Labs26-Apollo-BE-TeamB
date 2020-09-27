165aaf5c16b32281da82e0d72861a17092d34cd2
Jiayi Ren <jiayiren22@gmail.com>
2020-09-27 11:20:25 -0700

    add Swagger doc for QuestionController

--- src/main/java/com/lambdaschool/apollo/controllers/QuestionController.java
+++ src/main/java/com/lambdaschool/apollo/controllers/QuestionController.java
@@ -2,6 +2,9 @@ package com.lambdaschool.apollo.controllers;
 
 import com.lambdaschool.apollo.models.Question;
 import com.lambdaschool.apollo.services.QuestionService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,12 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
+    @ApiOperation(value = "List all questions")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved lsit", response = Question.class),
+            @ApiResponse(code = 401, message = "Not authorized"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @GetMapping(value = "/questions", produces = {"application/json"})
     public ResponseEntity<?> getAllQuestions() {
         List<Question> questions = questionService.findAllQuestions();
