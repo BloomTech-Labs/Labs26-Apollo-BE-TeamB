34ea5f00ee6ed8e8c2e8cf33265e77e599d2eaf9
Jiayi Ren <jiayiren22@gmail.com>
2020-09-27 11:20:35 -0700

    add Swagger doc for ContextController

--- src/main/java/com/lambdaschool/apollo/controllers/ContextController.java
+++ src/main/java/com/lambdaschool/apollo/controllers/ContextController.java
@@ -2,6 +2,9 @@ package com.lambdaschool.apollo.controllers;
 
 import com.lambdaschool.apollo.models.Context;
 import com.lambdaschool.apollo.services.ContextService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +22,24 @@ public class ContextController {
     @Autowired
     private ContextService contextService;
 
+    @ApiOperation(value = "List all context types")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list", response = Context.class, responseContainer = "List"),
+            @ApiResponse(code = 401, message = "Not authorized"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @GetMapping(value = "/contexts", produces = {"application/json"})
     public ResponseEntity<?> getAllContexts() {
         List<Context> myContexts = contextService.findAll();
-        return new ResponseEntity<>(myContexts,HttpStatus.OK);
+        return new ResponseEntity<>(myContexts, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get context type by Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved context type", response = Context.class),
+            @ApiResponse(code = 401, message = "Not authorized"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @GetMapping(value = "/contexts/{contextId}", produces = {"application/json"})
     public ResponseEntity<?> getContextById(@PathVariable Long contextId) {
         Context context = contextService.findById(contextId);
