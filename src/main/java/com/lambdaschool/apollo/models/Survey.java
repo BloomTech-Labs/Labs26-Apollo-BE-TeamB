0157da89e87a6b1985410818e0fa13d3fdff4571
Jiayi Ren <jiayiren22@gmail.com>
2020-09-27 11:19:30 -0700

    add Swagger doc for Survey

--- src/main/java/com/lambdaschool/apollo/models/Survey.java
+++ src/main/java/com/lambdaschool/apollo/models/Survey.java
@@ -1,11 +1,14 @@
 package com.lambdaschool.apollo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@ApiModel(value = "Survey", description = "This entity is a combination of surveys and survey requests")
 @Entity
 @Table(name = "surveys")
 public class Survey extends Auditable {
@@ -13,19 +16,23 @@ public class Survey extends Auditable {
     /**
      * The primary key (long) of the surveys table.
      */
+    @ApiModelProperty(name = "Survey id", value = "primary key for survey", required = true, example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long surveyid;
 
+    @ApiModelProperty(name = "Topic id", value = "Topic id for this survey request, can be null if this is used as survey", allowEmptyValue = true)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "topicId")
     @JsonIgnoreProperties(value = {"surveysrequests", "owner", "defaultsurveyid", "users"}, allowSetters = true)
     private Topic topic;
 
+    @ApiModelProperty(name = "Topic id", value = "Topic id ")
     @OneToMany(mappedBy = "defaultsurvey", cascade = CascadeType.ALL)
     @JsonIgnoreProperties(value = {"defaultsurvey", "survey"}, allowSetters = true)
     private List<Topic> defaulttopic;
 
+    @ApiModelProperty(name = "Question id", value = "Question associated with this survey")
     @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnoreProperties(value = "survey", allowSetters = true)
     private List<Question> questions = new ArrayList<>();
