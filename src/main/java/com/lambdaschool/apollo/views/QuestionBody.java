248fa64be63421fdaab07437913e8a8609bbeed8
Caleb <bargercj20@hotmai.com>
2020-09-28 17:23:18 -0700

    tweaks for QuestionBody view

--- src/main/java/com/lambdaschool/apollo/views/QuestionBody.java
+++ src/main/java/com/lambdaschool/apollo/views/QuestionBody.java
@@ -1,2 +1,40 @@
-package com.lambdaschool.apollo.views;public class QuestionBody {
+package com.lambdaschool.apollo.views;
+
+import javax.persistence.Entity;
+
+public class QuestionBody {
+    private String body;
+    private long questionid;
+
+    public QuestionBody() {
+    }
+
+    public QuestionBody(String body, long questionid) {
+        this.body = body;
+        this.questionid = questionid;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getQuestionid() {
+        return questionid;
+    }
+
+    public void setQuestionid(long questionid) {
+        this.questionid = questionid;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionBody{" +
+                "body='" + body + '\'' +
+                ", questionid=" + questionid +
+                '}';
+    }
 }
