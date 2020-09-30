6428db40d5ba4b78a56d368cd2df4876dbbe9583
Jiayi Ren <jiayiren22@gmail.com>
2020-09-30 11:47:35 -0700

    ContextService Test: findAll Tested

---
+++ src/test/java/com/lambdaschool/apollo/services/ContextServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.lambdaschool.apollo.services;
+
+import com.lambdaschool.apollo.ApolloApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApolloApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ContextServiceImplTest {
+
+    @Autowired
+    private ContextService contextService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void a_findAll() {
+        assertEquals(5, contextService.findAll().size());
+    }
+
+    @Test
+    public void b_findById() {
+    }
+
+    @Test
+    public void c_findByDescription() {
+    }
+
+    @Test
+    public void d_save() {
+    }
+}
