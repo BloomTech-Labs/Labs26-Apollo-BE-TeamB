67a5587fb2fda22f0b6b6387fdc4cbb2129de50d
caleb barger <bargercj20@hotmail.com>
2020-09-30 13:09:13 -0700

    auditable only includes getters for relevent fields

--- src/main/java/com/lambdaschool/apollo/models/Auditable.java
+++ src/main/java/com/lambdaschool/apollo/models/Auditable.java
@@ -57,35 +57,12 @@ abstract class Auditable {
     @Temporal(TIMESTAMP)
     protected Date lastModifiedDate;
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
     public Date getCreatedDate() {
         return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(String lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
     }
 
     public Date getLastModifiedDate() {
         return lastModifiedDate;
     }
 
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
 }
