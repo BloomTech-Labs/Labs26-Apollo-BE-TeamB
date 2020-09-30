5a61f7c76708f4b9b102df92a1b04ed7b866923b
caleb barger <bargercj20@hotmail.com>
2020-09-30 13:05:26 -0700

    auditable now has getters and setters for its fields, this is key in getting those fields to show up in the response body even though they were already inside the database

--- src/main/java/com/lambdaschool/apollo/models/Auditable.java
+++ src/main/java/com/lambdaschool/apollo/models/Auditable.java
@@ -56,4 +56,36 @@ abstract class Auditable {
     @LastModifiedDate
     @Temporal(TIMESTAMP)
     protected Date lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
 }
