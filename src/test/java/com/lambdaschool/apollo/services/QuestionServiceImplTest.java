45892ddcc7667d55a3654bdb3eff3ecc58d80984
Justin Lohner <lohner4@gmail.com>
2020-10-06 14:26:45 -0600

    added test files for question service and controller

---
+++ src/test/java/com/lambdaschool/apollo/services/QuestionServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.lambdaschool.apollo.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findAllQuestions() {
+    }
+
+    @Test
+    public void findAllBySurveyId() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
