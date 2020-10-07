40c558e1c93f02f38ff05498ebf7770da2880908
Justin Lohner <lohner4@gmail.com>
2020-10-07 10:27:56 -0600

    added order to how tests are executed, delete test not working currently because there is something wrong with the delete function in QuestionService, so we will have to fix that before the test will pass

--- src/test/java/com/lambdaschool/apollo/services/QuestionServiceImplTest.java
+++ src/test/java/com/lambdaschool/apollo/services/QuestionServiceImplTest.java
@@ -6,16 +6,20 @@ import com.lambdaschool.apollo.models.Survey;
 import com.lambdaschool.apollo.views.QuestionType;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApolloApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class QuestionServiceImplTest {
     @Autowired
     private QuestionService questionService;
@@ -30,28 +34,28 @@ public class QuestionServiceImplTest {
     }
 
     @Test
-    public void findById() {
+    public void a_findById() {
         assertEquals("Leader Question 1", questionService.findById(29).getBody());
     }
 
     @Test
-    public void findAllQuestions() {
+    public void b_findAllQuestions() {
         assertEquals(5, questionService.findAllQuestions().size());
     }
 
     @Test
-    public void findAllBySurveyId() {
+    public void c_findAllBySurveyId() {
         assertEquals(3, questionService.findAllBySurveyId(9).size());
     }
 
     @Test
-    public void delete() {
-        questionService.delete(33);
+    public void d_delete() {
+        questionService.delete(29);
         assertEquals(4, questionService.findAllQuestions().size());
     }
 
     @Test
-    public void save() {
+    public void e_save() {
         Survey s1 = new Survey();
         s1.setSurveyId(9);
         Question newQuestion = new Question("Test Question", false, QuestionType.TEXT, s1);
@@ -60,7 +64,7 @@ public class QuestionServiceImplTest {
     }
 
     @Test
-    public void update() {
+    public void f_update() {
         // asserted to be null until update function is actually added
         assertNull(questionService.update(new Question()));
     }
