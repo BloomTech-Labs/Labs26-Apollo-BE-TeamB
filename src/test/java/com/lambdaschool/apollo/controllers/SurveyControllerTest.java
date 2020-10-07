9d148e8afef21882f6067deb358820da3a5cd11e
Justin Lohner <lohner4@gmail.com>
2020-10-07 10:48:37 -0600

    added test files for survey controller and service

---
+++ src/test/java/com/lambdaschool/apollo/controllers/SurveyControllerTest.java
@@ -0,0 +1,42 @@
+package com.lambdaschool.apollo.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SurveyControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void createNewSurvey() {
+    }
+
+    @Test
+    public void createNewResponse() {
+    }
+
+    @Test
+    public void getAllSurveys() {
+    }
+
+    @Test
+    public void testGetAllSurveys() {
+    }
+
+    @Test
+    public void createSurveyRequest() {
+    }
+
+    @Test
+    public void getResponses() {
+    }
+}
---
+++ src/test/java/com/lambdaschool/apollo/services/SurveyServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.lambdaschool.apollo.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SurveyServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void saveRequest() {
+    }
+
+    @Test
+    public void findAllSurveys() {
+    }
+}
