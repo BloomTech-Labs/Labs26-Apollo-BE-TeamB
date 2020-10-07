94791e7d514ce8a3442afca02f0a33399dac7aac
Justin Lohner <lohner4@gmail.com>
2020-10-06 21:17:18 -0600

    got all tests passing in QuestionControllerTest

--- src/test/java/com/lambdaschool/apollo/controllers/QuestionControllerTest.java
+++ src/test/java/com/lambdaschool/apollo/controllers/QuestionControllerTest.java
@@ -2,6 +2,7 @@ package com.lambdaschool.apollo.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.apollo.ApolloApplication;
+import com.lambdaschool.apollo.handlers.HelperFunctions;
 import com.lambdaschool.apollo.models.Context;
 import com.lambdaschool.apollo.models.Question;
 import com.lambdaschool.apollo.models.Survey;
@@ -41,32 +42,45 @@ public class QuestionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    HelperFunctions helperFunctions;
+
     @MockBean
     private QuestionService questionService;
 
     private List<Question> questionList;
+    private List<Question> leaderQuestionList;
 
     @Before
     public void setUp() throws Exception {
         questionList = new ArrayList<>();
+        leaderQuestionList = new ArrayList<>();
 
-        Question question1 = new Question("Leader Question 1", true, QuestionType.TEXT, new Survey());
+        Survey s1 = new Survey();
+        s1.setSurveyId(9);
+
+        Survey s2 = new Survey();
+        s2.setSurveyId(11);
+
+        Question question1 = new Question("Leader Question 1", true, QuestionType.TEXT, s1);
         questionList.add(question1);
+        leaderQuestionList.add(question1);
         question1.setQuestionId(29);
 
-        Question question2 = new Question("Leader Question 2", true, QuestionType.TEXT, new Survey());
+        Question question2 = new Question("Leader Question 2", true, QuestionType.TEXT, s1);
         questionList.add(question2);
+        leaderQuestionList.add(question2);
         question2.setQuestionId(30);
 
-        Question question3 = new Question("Member Question 1", false, QuestionType.TEXT, new Survey());
+        Question question3 = new Question("Member Question 1", false, QuestionType.TEXT, s2);
         questionList.add(question3);
         question3.setQuestionId(31);
 
-        Question question4 = new Question("Member Question 2", false, QuestionType.TEXT, new Survey());
+        Question question4 = new Question("Member Question 2", false, QuestionType.TEXT, s2);
         questionList.add(question4);
         question4.setQuestionId(32);
 
-        Question question5 = new Question("Member Question 3", false, QuestionType.TEXT, new Survey());
+        Question question5 = new Question("Member Question 3", false, QuestionType.TEXT, s2);
         questionList.add(question5);
         question5.setQuestionId(33);
     }
@@ -82,7 +96,6 @@ public class QuestionControllerTest {
         Mockito.when(questionService.findAllQuestions()).thenReturn(questionList);
 
         RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
-
         MvcResult r = mockMvc.perform(rb).andReturn();
         String tr = r.getResponse().getContentAsString();
 
@@ -97,5 +110,21 @@ public class QuestionControllerTest {
 
     @Test
     public void getLeaderQuestionsBySurveyId() throws Exception {
+        String apiUrl = "/questions/leader/9";
+
+        Mockito.when(helperFunctions.isLeaderQuestion(questionService.findAllBySurveyId(9)))
+            .thenReturn(leaderQuestionList);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(leaderQuestionList);
+
+        System.out.println("Expect: " + er);
+        System.out.println("Actual: " + tr);
+
+        assertEquals("Rest API Returns List", er, tr);
     }
 }
