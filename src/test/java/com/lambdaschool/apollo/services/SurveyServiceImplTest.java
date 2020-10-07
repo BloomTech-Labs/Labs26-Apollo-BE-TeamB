4e2f96fe06c8862b012a7e9541caab6d9d5d7096
Justin Lohner <lohner4@gmail.com>
2020-10-07 16:51:49 -0600

    got findAllSurveys test passing

--- src/test/java/com/lambdaschool/apollo/services/SurveyServiceImplTest.java
+++ src/test/java/com/lambdaschool/apollo/services/SurveyServiceImplTest.java
@@ -1,15 +1,29 @@
 package com.lambdaschool.apollo.services;
 
+import com.lambdaschool.apollo.ApolloApplication;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApolloApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SurveyServiceImplTest {
+    @Autowired
+    private SurveyService surveyService;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
@@ -18,6 +32,7 @@ public class SurveyServiceImplTest {
 
     @Test
     public void findById() {
+        // assertEquals(10, surveyService.findById(9).getTopic().getTopicId());
     }
 
     @Test
@@ -34,5 +49,6 @@ public class SurveyServiceImplTest {
 
     @Test
     public void findAllSurveys() {
+        assertEquals(11, surveyService.findAllSurveys().size());
     }
 }
