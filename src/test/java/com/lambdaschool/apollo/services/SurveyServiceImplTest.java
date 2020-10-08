bbe88fdbf1aafe83d4c909e332edc1dee3a2c978
Justin Lohner <lohner4@gmail.com>
2020-10-07 18:19:56 -0600

    added letters for ordering the tests, two functions still not passing though

--- src/test/java/com/lambdaschool/apollo/services/SurveyServiceImplTest.java
+++ src/test/java/com/lambdaschool/apollo/services/SurveyServiceImplTest.java
@@ -3,6 +3,9 @@ package com.lambdaschool.apollo.services;
 import com.lambdaschool.apollo.ApolloApplication;
 import com.lambdaschool.apollo.models.Survey;
 import com.lambdaschool.apollo.models.Topic;
+import com.lambdaschool.apollo.repository.SurveyRepository;
+import com.lambdaschool.apollo.views.QuestionType;
+import com.lambdaschool.apollo.views.SurveyQuestion;
 import com.lambdaschool.apollo.views.TopicFrequency;
 import org.junit.After;
 import org.junit.Before;
@@ -10,12 +13,18 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApolloApplication.class)
@@ -34,28 +43,36 @@ public class SurveyServiceImplTest {
     }
 
     @Test
-    public void findById() {
+    public void a_findById() {
         assertEquals(9, surveyService.findById(9).getSurveyId());
     }
 
     @Test
-    public void delete() {
+    public void b_delete() {
         // surveyService.delete(23);
         // assertEquals(10, surveyService.findAllSurveys().size());
     }
 
     @Test
-    public void save() {
+    public void c_save() {
         surveyService.save(new Survey());
         assertEquals(12, surveyService.findAllSurveys().size());
     }
 
     @Test
-    public void saveRequest() {
+    public void d_saveRequest() {
+        // Topic saveTopic = surveyService.findById(9).getTopic();
+        // Topic saveTopic = new Topic("test topic", userService.findUserById(4), surveyService.findById(9), TopicFrequency.DAILY);
+        // List<SurveyQuestion> newQuestions = new ArrayList<>();
+        // newQuestions.add(new SurveyQuestion("tesy survey question", QuestionType.TEXT, false));
+        // // System.out.println(saveTopic);
+        // // System.out.println(newQuestions);
+        // surveyService.saveRequest(newQuestions, saveTopic);
+        // assertEquals(1, surveyService.findById(9).getTopic().getSurveysrequests().size());
     }
 
     @Test
-    public void findAllSurveys() {
-        assertEquals(11, surveyService.findAllSurveys().size());
+    public void e_findAllSurveys() {
+        assertEquals(12, surveyService.findAllSurveys().size());
     }
 }
