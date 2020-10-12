787cd302baa431b57d19e6c04ad3530f06811022
jiayi-ren <jiayiren95@gmail.com>
2020-10-12 15:03:39 -0700

    add custom query: findAnswerByQuestionIdAndUserId

--- src/main/java/com/lambdaschool/apollo/repository/AnswerRepository.java
+++ src/main/java/com/lambdaschool/apollo/repository/AnswerRepository.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.apollo.repository;
 
 import com.lambdaschool.apollo.models.Answer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.List;
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
 
     List<Answer> findBySurvey_surveyid(long surveyid);
+
+    @Query(value = "SELECT * FROM Answers WHERE questionid = :questionid AND userid = :userid", nativeQuery = true)
+    Answer findAnswerByQuestionIdAndUserId(long questionid, long userid);
 }
