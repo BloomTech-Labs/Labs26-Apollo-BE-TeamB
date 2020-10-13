7465ad163ffcd645aa8a72476b989bf46c109cb8
Caleb <bargercj20@hotmail.com>
2020-10-13 13:09:57 -0700

    added a removequestion service to survey

--- src/main/java/com/lambdaschool/apollo/services/SurveyService.java
+++ src/main/java/com/lambdaschool/apollo/services/SurveyService.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.apollo.services;
 
+import com.lambdaschool.apollo.models.Question;
 import com.lambdaschool.apollo.models.Survey;
 import com.lambdaschool.apollo.models.Topic;
 import com.lambdaschool.apollo.views.SurveyQuestion;
@@ -17,4 +18,6 @@ public interface SurveyService {
     Survey save(Survey survey);
 
     Survey saveRequest(List<SurveyQuestion> questions, Topic topic);
+
+    void removeQuestion(Survey survey, long id);
 }
