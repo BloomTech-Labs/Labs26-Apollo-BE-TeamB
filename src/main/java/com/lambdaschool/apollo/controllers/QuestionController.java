f031a1343f344f692c9533fe8ae40e2c3cf649fc
Caleb <bargercj20@hotmail.com>
2020-10-13 13:10:35 -0700

    wrote a remove question endpoint

--- src/main/java/com/lambdaschool/apollo/controllers/QuestionController.java
+++ src/main/java/com/lambdaschool/apollo/controllers/QuestionController.java
@@ -9,10 +9,7 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,4 +43,10 @@ public class QuestionController {
         return new ResponseEntity<>(questions, HttpStatus.OK);
     }
 
+//    @DeleteMapping(value = "/question/{questionid}") <-- Ideally we will make this available once the service works
+//    public ResponseEntity<?> deleteQuestionById(@PathVariable
+//                                                long questionid) {
+//        questionService.delete(questionid);
+//        return new ResponseEntity<>(null, HttpStatus.GONE);
+//    }
 }
