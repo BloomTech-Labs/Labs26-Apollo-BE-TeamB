c36402006ffef096f62744bb461cafc843adf553
jiayi-ren <jiayiren95@gmail.com>
2020-10-20 15:04:42 -0700

    add swagger doc for delete question endpoint and authentication

--- src/main/java/com/lambdaschool/apollo/controllers/QuestionController.java
+++ src/main/java/com/lambdaschool/apollo/controllers/QuestionController.java
@@ -2,13 +2,16 @@ package com.lambdaschool.apollo.controllers;
 
 import com.lambdaschool.apollo.handlers.HelperFunctions;
 import com.lambdaschool.apollo.models.Question;
+import com.lambdaschool.apollo.models.User;
 import com.lambdaschool.apollo.services.QuestionService;
+import com.lambdaschool.apollo.services.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +25,9 @@ public class QuestionController {
 
     @Autowired
     HelperFunctions helperFunctions;
+
+    @Autowired
+    UserService userService;
 
     @ApiOperation(value = "List all questions")
     @ApiResponses(value = {
@@ -37,16 +43,22 @@ public class QuestionController {
 
     @GetMapping(value = "/leader/{surveyid}", produces = "application/json")
     public ResponseEntity<?> getLeaderQuestionsBySurveyId(@PathVariable
-                                                          long surveyid) {
+                                                                  long surveyid) {
         List<Question> questions = helperFunctions.isLeaderQuestion(
                 questionService.findAllBySurveyId(surveyid));
         return new ResponseEntity<>(questions, HttpStatus.OK);
     }
 
-//    @DeleteMapping(value = "/question/{questionid}") <-- Ideally we will make this available once the service works
-//    public ResponseEntity<?> deleteQuestionById(@PathVariable
-//                                                long questionid) {
-//        questionService.delete(questionid);
-//        return new ResponseEntity<>(null, HttpStatus.GONE);
-//    }
+    @ApiOperation(value = "Delete a question")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully delete the question"),
+            @ApiResponse(code = 401, message = "Not authorized"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @DeleteMapping(value = "/question/{questionid}")
+    public ResponseEntity<?> deleteQuestionById(@PathVariable long questionid, Authentication authentication) {
+        User user = userService.findByOKTAUserName(authentication.getName());
+        questionService.delete(questionid, user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
